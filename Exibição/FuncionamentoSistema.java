9e7ad366441d5cfd4af1afd546d4a43b84ba1f74
Ana Júlia <111990344+Anajukks@users.noreply.github.com>
2023-01-26 16:19:26 -0300

    Add files via upload

---
+++ Exibição/FuncionamentoSistema.java
@@ -0,0 +1,10 @@
+public class FuncionamentoSistema{
+    // pq não criar uma função na classe filmes?
+    // pq voltar n faz parte de filmes, como a função de filme é ter seus atributos e metodos relacionados ao filme,
+    // nao fazia sentido colocar algo q é do funcionamento interno do programa na classe.
+    public void Retornar(){
+        System.out.println("loading....");
+        System.out.println("*retorna para tela inicial*"); 
+        // aqui dentro teria q ter algo voltando para o inicio do codigo
+    }
+}
